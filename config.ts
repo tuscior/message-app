8012eef61903544fa8b7c7353c9a4e61193c5e8b
tuscior <tluscik.k@gmail.com>
2020-06-07 18:49:07 +0200

    working on docker hub

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    DB_URL: process.env.DB_URL || 'mongodb://mongodb/',
+    DB_URL: process.env.DB_URL || 'mongodb://mongo:27017',
     PORT: process.env.PORT || 8080,
     VERSION: process.env.VERSION || '1.0.0',
     REQUEST_LIMIT: 25
